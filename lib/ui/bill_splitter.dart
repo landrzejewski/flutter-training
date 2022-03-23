2ce923ac612fe48a6a827ac88e0a7f4ab770844b
landrzejewski <landrzejewski.poczta@gmail.com>
2022-03-23 15:06:30 +0100

    base

--- lib/ui/bill_splitter.dart
+++ lib/ui/bill_splitter.dart
@@ -30,14 +30,13 @@ class _BillSplitterState extends State<BillSplitter> {
             Container(
               padding: const EdgeInsets.all(defaultSpace),
               decoration: BoxDecoration(
-                  border: Border.all(color: mainColor),
+                  border: Border.all(color: Colors.grey.shade400, width: 1),
                   borderRadius: BorderRadius.circular(defaultRadius)),
               child: Column(
                 children: [
                   TextField(
-                    controller: TextEditingController(text: "$_billValue zł"),
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
+                    controller: TextEditingController(text: "$_billValue"),
+                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
                     decoration: const InputDecoration(labelText: "Bill value:"),
                     onChanged: _onBillValueChanged,
                   ),
@@ -45,32 +44,7 @@ class _BillSplitterState extends State<BillSplitter> {
                   Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text("Number of persons:", style: defaultTextStyle),
-                      Row(children: [
-                        InkWell(
-                          onTap: _onRemovePerson,
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: defaultButtonSize,
-                            height: defaultButtonSize,
-                            margin: const EdgeInsets.all(smallSpace),
-                            decoration: BoxDecoration(color: mainColor, borderRadius: BorderRadius.circular(defaultRadius)),
-                            child: const Text("-", style: defaultButtonTextStyle),
-                          ),
-                        ),
-                        Text("$_numberOfPersons", style: defaultTextStyle),
-                        InkWell(
-                          onTap: _onAddPerson,
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: defaultButtonSize,
-                            height: defaultButtonSize,
-                            margin: const EdgeInsets.all(smallSpace),
-                            decoration: BoxDecoration(color: mainColor, borderRadius: BorderRadius.circular(defaultRadius)),
-                            child: const Text("+", style: defaultButtonTextStyle),
-                          ),
-                        )
-                      ],
-                      )
+                      NumberField(onChange: _onNumberOfPersonsChange)
                     ],
                   ),
                   Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -96,30 +70,98 @@ class _BillSplitterState extends State<BillSplitter> {
                 ],
               ),
             ),
+            const SizedBox(height: defaultSpace),
+            Center(child: Text("Value per person: ${_valuePerPerson()}", style: defaultTextStyle))
           ],
         ),
       ),
     );
   }
 
-  void _onBillValueChanged(String billValue) {}
-
-  void _onRemovePerson() {
-    if (_numberOfPersons > 1) {
-      setState(() => _numberOfPersons--);
+  void _onBillValueChanged(String billValue) {
+    try {
+      _billValue = double.parse(billValue);
+    } catch (exception) {
+      debugPrint("Invalid bill value");
     }
   }
 
-  void _onAddPerson() {
-    setState(() => _numberOfPersons++);
+  void _onNumberOfPersonsChange(int numberOfPersons) {
+    setState(() => _numberOfPersons = numberOfPersons);
   }
 
   double _totalTip() {
-    return 0.0;
+    return (_billValue * _tipPercentage) / 100;
   }
 
   void _onTipPercentageChange(double value) {
     setState(() => _tipPercentage = value.round());
+  }
+
+  String _valuePerPerson() {
+    final result = ((_totalTip() + _billValue) / _numberOfPersons).toStringAsFixed(2);
+    return "$result zł";
+  }
+
+}
+
+class NumberField extends StatefulWidget {
+  final void Function(int)? onChange;
+  const NumberField({Key? key, this.onChange}) : super(key: key);
+
+  @override
+  State<NumberField> createState() => _NumberFieldState(onChange);
+}
+
+class _NumberFieldState extends State<NumberField> {
+
+  final void Function(int)? onChange;
+  var _value = 1;
+
+  _NumberFieldState(this.onChange);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+        CustomButton(text: "-", onTap: _onDecrement),
+        Text("$_value", style: defaultTextStyle),
+        CustomButton(text: "+", onTap: _onIncrement)
+    ]);
+  }
+
+  void _onDecrement() {
+    if (_value > 1) {
+      setState(() => _value--);
+      onChange?.call(_value);
+    }
+  }
+
+  void _onIncrement() {
+    setState(() => _value++);
+    onChange?.call(_value);
+  }
+
+}
+
+
+class CustomButton extends StatelessWidget {
+  final void Function() onTap;
+  final String text;
+  const CustomButton({Key? key, required this.text, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  InkWell(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: defaultButtonSize,
+        height: defaultButtonSize,
+        margin: const EdgeInsets.all(smallSpace),
+        decoration: BoxDecoration(color: mainColor, borderRadius: BorderRadius.circular(defaultRadius)),
+        child: Text(text, style: defaultButtonTextStyle),
+      ),
+    );
   }
 
 }
